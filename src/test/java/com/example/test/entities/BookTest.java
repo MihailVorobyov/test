564e769a17a28b430e06cc9440442f0411634956
Mihail_Vorobyov <ptichkinm@mail.ru>
2022-02-24 11:22:17 +0300

    Созданы тесты для Book

---
+++ src/test/java/com/example/test/entities/BookTest.java
@@ -0,0 +1,67 @@
+package com.example.test.entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+public class BookTest {
+	
+	private Book book;
+	
+	@BeforeEach
+	public void setUp() {
+		book = new Book();
+	}
+	
+	@Test
+	void shouldSetAndGetId() {
+		long id = 9L;
+		book.setId(id);
+		
+		assertEquals(id, book.getId());
+	}
+	
+	@Test
+	void shouldSetAndGetISBN() {
+		String ISBN = "123-456-7";
+		book.setISBN(ISBN);
+		
+		assertEquals(ISBN, book.getISBN());
+	}
+	
+	@Test
+	void shouldSetAndGetTitle() {
+		String title = "Анна Каренина";
+		book.setTitle(title);
+		
+		assertEquals(title, book.getTitle());
+	}
+	
+	@Test
+	void shouldSetAndGetPublicationYear() {
+		int publicationYear = 1860;
+		book.setPublicationYear(publicationYear);
+		
+		assertEquals(publicationYear, book.getPublicationYear());
+	}
+	
+	@Test
+	void shouldSetAndGetGenre() {
+		String genre = "Роман";
+		book.setGenre(genre);
+		
+		assertEquals(genre, book.getGenre());
+	}
+	
+	@Test
+	void shouldSetAndGetAddDate() {
+		LocalDate addDate = LocalDate.of(2022, 02, 24);
+		book.setAddDate(addDate);
+		
+		assertEquals(24, book.getAddDate().getDayOfMonth());
+	}
+}
