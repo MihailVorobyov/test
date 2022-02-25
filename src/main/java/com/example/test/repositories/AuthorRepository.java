e3e27e8d8fa792b05961526bbf408424b4d3a5c7
Mihail_Vorobyov <ptichkinm@mail.ru>
2022-02-25 15:37:38 +0300

    Доработан AuthorRepository

--- src/main/java/com/example/test/repositories/AuthorRepository.java
+++ src/main/java/com/example/test/repositories/AuthorRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AuthorRepository extends CrudRepository<Author, Long> {
@@ -12,7 +13,17 @@ public interface AuthorRepository extends CrudRepository<Author, Long> {
 	@Override
 	List<Author> findAll();
 	
-	Author findFirstByLastName(String lastName);
+	@Override
+	<S extends Author> S save(S entity);
+	
+	@Override
+	void deleteById(Long aLong);
+	
+	@Override
+	boolean existsById(Long aLong);
+	
+	@Override
+	Optional<Author> findById(Long aLong);
 	
 	@Override
 	<S extends Author> List<S> saveAll(Iterable<S> entities);
